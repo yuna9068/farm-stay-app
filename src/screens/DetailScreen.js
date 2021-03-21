e5d1633c440b7b8c1a4eaca2f9577cf95f6bfb8c
Yuna <yuna9068@gmail.com>
2021-03-21 23:48:26 +0800

    feat: 新增農場詳細資訊

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import * as ImageHelpers from '../helpers/ImageHelpers.js';
 
 export default function DetailScreen(props) {
+  const farm = props.route.params.farm;
   return (
-    <View>
-      <Text>DetailScreen</Text>
-      <Text>props.route.params.farm: { props.route.params.farm }</Text>
-      <Button
-        title="前往 HomeScreen"
-        onPress={() => props.navigation.pop()}
+    <View style={styles.mainView}>
+      <Text style={styles.name}>{farm.Name}</Text>
+      <Text style={styles.info}>{farm.AddrDisplay}</Text>
+      <Text style={styles.info}>{farm.TelDisplay}</Text>
+      <Text style={styles.info}>{farm.Content}</Text>
+      <Image
+        source={ImageHelpers.checkImage(farm.Image)}
+        style={{width: 300, height: 300}}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  name: {
+    fontSize: 20,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  info: {
+    color: '#003e80',
+    marginBottom: 5,
+  },
+});
