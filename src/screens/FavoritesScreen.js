c5a832411c00dce5ef8ab65604a22bac056a015a
Yuna <yuna9068@gmail.com>
2021-03-22 23:16:58 +0800

    feat: 修改收藏景點頁面內文

--- src/screens/FavoritesScreen.js
+++ src/screens/FavoritesScreen.js
@@ -1,19 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function FavoritesScreen(props) {
   const farm = useState("farm from Favorites");
   return (
     <View>
-      <Text>FavoritesScreen</Text>
-      <Button
-        title="前往 DetailScreen"
-        onPress={() => {
-          props.navigation.push('Detail', {
-            farm,
-          })
-        }}
-      />
+      <Text style={{textAlign: 'center', margin: 20}}>收藏景點功能開發中</Text>
     </View>
   );
 }
