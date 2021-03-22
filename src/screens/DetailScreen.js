99daf31c7de8d8389fb27c8dd924a67c767007a4
Yuna <yuna9068@gmail.com>
2021-03-22 23:20:42 +0800

    feat: 修改景點詳細資訊頁的內容

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import * as ImageHelpers from '../helpers/ImageHelpers.js';
 
 export default function DetailScreen(props) {
   const farm = props.route.params.farm;
   return (
-    <View style={styles.mainView}>
-      <Text style={styles.name}>{farm.Name}</Text>
-      <Text style={styles.info}>{farm.AddrDisplay}</Text>
-      <Text style={styles.info}>{farm.TelDisplay}</Text>
-      <Text style={styles.info}>{farm.Content}</Text>
-      <Image
-        source={ImageHelpers.checkImage(farm.Image)}
-        style={{width: 300, height: 300}}
-      />
-    </View>
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.mainView}>
+        <Text style={styles.name}>{farm.Name}</Text>
+        <Text style={styles.info}>{farm.AddrDisplay}</Text>
+        <Text style={styles.info}>{farm.TelDisplay}</Text>
+        <Text style={styles.info}>{farm.Content}</Text>
+        <Image
+          source={ImageHelpers.checkImage(farm.Image)}
+          style={{width: 300, height: 300}}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
@@ -22,15 +24,19 @@ const styles = StyleSheet.create({
   mainView: {
     flex: 1,
     justifyContent: 'flex-start',
-    alignItems: 'center'
+    alignItems: 'center',
+    paddingVertical: 20,
+    paddingHorizontal: 20,
   },
   name: {
     fontSize: 20,
-    marginTop: 10,
+    lineHeight: 24,
     marginBottom: 10,
   },
   info: {
+    fontSize: 16,
+    lineHeight: 22,
     color: '#003e80',
-    marginBottom: 5,
+    marginBottom: 10,
   },
 });
